4b38544a7ad0696a9e2aeb6171a35b8437870dc4
Nicola <nikof.06@gmail.com>
2023-07-26 00:37:09 +0200

    Added empty JFX project
    Created with IntelliJ wizard (gradlew 8.0.2, oracle openjdk 19.0). Minor edits to build.gradle

---
+++ followme-bevilacqua/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module it.unicam.cs.pa.followmebevilacqua {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens it.unicam.cs.pa.followmebevilacqua to javafx.fxml;
+    exports it.unicam.cs.pa.followmebevilacqua;
+}
